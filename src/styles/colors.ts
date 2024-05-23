6abd253e10db0dd8d8e29dbb8a745ffdfa998782
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-05-23 14:19:39 +0200

    initial commit

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  lightPurple: '#B2A7F4',
+  purpleGray: '#595D7B',
+  midnightPurple: '#423C66',
+  midnightGray: '#4D6475',
+
+  salomon: '#FFDBCB',
+  stroke: '#E9EEF2'
+}
