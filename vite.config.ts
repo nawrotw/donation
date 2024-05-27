720a7857e933f1d2acbb426d4953500747071473
Wojciech Nawrot <wojtek.nawrot@gmail.com>
2024-05-27 12:19:50 +0200

    GitHub tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import GithubActionsReporter from 'vitest-github-actions-reporter';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "happy-dom",
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    outputFile: {
+      junit: "test-results.xml",
+    },
+    reporters: process.env.GITHUB_ACTIONS
+      ? [
+        "junit",
+        new GithubActionsReporter({
+          hideStackTrace: true,
+          trimRepositoryPrefix: true,
+        }),
+      ]
+      : "default",
   }
 })
